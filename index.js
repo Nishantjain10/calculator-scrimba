ceabddbe61fdaf5d9901643004fda9644a1c3b57
Nishant Jain <89480234+Nishantjain10@users.noreply.github.com>
2022-11-15 22:53:29 +0530

    Calculator In Scrimba

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
+//Made with Scrimba
+
 let sumEl = document.getElementById("sum-el")
 
 function add() {
